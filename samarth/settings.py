ddbdd3f0e699520c81a6f54dd32ed33be064bc97
KAPIL RATHOD <kapilrathod1234@gmail.com>
2021-04-27 15:11:48 +0300

    heroku commit

--- samarth/settings.py
+++ samarth/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'zkgtf(x$659@h$4an@wn8r%fpp$ice1eh2%btw(63bv7hca)0i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
